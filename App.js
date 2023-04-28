d93b666c3a0725c5f56f9bbd0b344aceab431f16
Marksedrak <81726544+Marksedrak@users.noreply.github.com>
2023-04-27 23:38:20 -0400

    Added a Root Stack Navigator to navigate between Pages

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./HomeScreen";
+import Login from "./Login";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerTitle: null,
+          headerBackTitleVisible: true,
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ HomeScreen.js
@@ -0,0 +1,62 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Dimensions, Pressable } from "react-native";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
+import { noramlize } from "./FontDynam";
+
+export default function HomeScreen({ navigation }) {
+  // State to determine when font is loaded
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        Pacifico: require("./assets/Fonts/Pacifico-Regular.ttf"),
+      });
+      setFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.welcomText, styles.text, styles.shadow]}>
+        Welcome to my LearnPress Training App!
+      </Text>
+      <Text style={[styles.text, styles.shadow]}>*Press the Button to begin*</Text>
+      <Pressable onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.text}>Button Here</Text>
+      </Pressable>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  welcomText: {
+    fontFamily: "Pacifico",
+    fontSize: noramlize(22),
+    textAlign: "center",
+    marginTop: Dimensions.get("window").height * 0.05,
+  },
+  text: {
+    color: "white",
+    margin: 5,
+  },
+  shadow: {
+    textShadowColor: 'rgba(100, 100, 100, 0.85)',
+    textShadowOffset: {width: -2, height: 1},
+    textShadowRadius: 5
+  }
+});
